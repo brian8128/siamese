69264967fa4f9b7b8f84ff63fea5a018fa573307
Ubuntu <ubuntu@ip-172-31-37-120.us-west-2.compute.internal>
2016-04-23 20:33:06 +0000

    Add prod settings

--- settings/prod.py
+++ settings/prod.py
@@ -1,11 +1,29 @@
-PROJECT_HOME = "TODO"
+PROJECT_HOME = "/home/ubuntu/siamese"
 
-# Settings for a gpu run. The GPU will carry out the computations an order of magnitude
-# faster so we can have the most epochs and the most filters here
+# Settings for a local run.  More computational muscle than dev but less than if we
+# were actually running on a gpu
 
 # Settings for vanilla nn
 NB_EPOCH = 10
 
 # Settings for cnn
-NB_EPOCH_CONV = 20
-NB_CONV_FILTERS = 32
+L1_FILTERS = 32
+L2_FILTERS = 64
+
+DROPOUT = True
+CONVO_DROPOUT_FRACTION = 0.2
+DROPOUT_FRACTION = 0.6
+
+# Dimension of the embedding space. Here it is artifically small so we can visulaize it
+EMBEDDING_DIM = 64
+
+FULLY_CONNECTED_SIZE = 128
+
+# Learning Rate. Here we make it smaller because we seem to diverge with the normal learning
+# rate and such a small embedding space
+LEARNING_RATE = 0.001
+OPTIMIZER = 'sgd'
+
+# Margin for the contrastive loss function.  How far apart two observations from different
+# classes need to be before the error is zero
+MARGIN = 0.5
